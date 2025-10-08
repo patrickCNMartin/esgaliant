75b1ae31e29aa2166bcfcc760923ba458fbd7a17
patrickCNMartin <pcnmartin@gmail.com>
2025-10-08 15:27:24 -0700

    adding stuff

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# docs/conf.py
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'esgaliant'
+author = 'Patrick CN Martin'
+release = '0.0.1'
+
+extensions = [
+    'sphinx.ext.autodoc',      # Auto-generate from docstrings
+    'sphinx.ext.napoleon',     # Support Google/NumPy style
+    'sphinx.ext.viewcode',     # Add source code links
+    'sphinx.ext.intersphinx',  # Link to other docs
+    'sphinx_autodoc_typehints', # Better type hint display
+]
+
+# Theme
+html_theme = 'sphinx_rtd_theme'  # Read the Docs theme
+
+# Napoleon settings for Google/NumPy style
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
---
+++ esgaliant/__init__.py
@@ -0,0 +1,15 @@
+"""
+Package initialization.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Patrick CN Martin"
+__email__ = "patrick.martin@cshs.org"
+
+# Import main components to make them available at package level
+
+
+# __all__ = [
+#     "hello_world",
+#     "greet",
+# ]
